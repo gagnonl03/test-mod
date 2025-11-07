c8edef916b249a2c50d2a500597a29593c4876b9
gagnonl03 <gagnonl@msoe.edu>
2025-11-07 14:05:53 -0600

    why

--- src/main/java/com/example/BookItem.java
+++ src/main/java/com/example/BookItem.java
@@ -1,6 +1,5 @@
 package com.example;
 
-import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
